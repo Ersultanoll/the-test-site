aed21370850e9779c5176c894c3fb3f154d586a8
Ersultan <samadok123.com@gmail.com>
2023-12-14 22:43:19 +0600

    second

--- index.js
+++
@@ -1,7 +0,0 @@
-let burger = document.getElementById('burger')
-let navbar = document.getElementById('navbar')
-
-burger.onclick = function () {
-	navbar.classList.add('add-nav');
-	navbar.classList.add('add-nav ul')
-}
